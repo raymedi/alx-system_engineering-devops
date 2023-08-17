b80a27413bf2dae35d9b0ae1e200f4affa83b6ab
raymedi <rahmonoluwashola@gmail.com>
2023-08-17 04:24:36 -0700

    puppet

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,4 @@
+# puppet manifest for setting limit of nginx
+exec { '/etc/default/nginx':
+  command => '/bin/echo ULIMIT="-n 5000" > /etc/default/nginx && sudo /usr/bin/service nginx restart',
+}
---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,4 @@
+# changing holberton hard and soft limits
+exec { 'change_holberton_limits':
+  command => '/bin/sed -i "s/nofile /nofile 1000/g" /etc/security/limits.conf'
+}
